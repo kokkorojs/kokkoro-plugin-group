a733c96130ab4c279cb5fd41778c63aecb05ba16
yuki <dc_yuki@qq.com>
2022-03-14 23:21:04 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # kokkoro-group
 
-群管理，时不时来塞口球
+> 群管理，时不时来塞口球
+
+## 安装
+
+``` shell
+# 切换至 bot 目录
+cd bot
+
+# 安装 npm 包
+npm i kokkoro-plugin-group
+```
+
+在 [kokkoro](https://github.com/kokkorojs/kokkoro) 成功运行并登录后，发送 `>enable group` 即可启用插件
+使用 `>group <key> <value>` 可修改当前群聊的插件参数，例如修改申请头衔等级限制 `>group title_level 1`
+
+## 参数
+
+``` typescript
+interface GroupOption {
+  // 申请头衔等级限制
+  title_level: number;
+}
+```
+
+## 等级
+
+- level 0 群成员（随活跃度提升）
+- level 1 群成员（随活跃度提升）
+- level 2 群成员（随活跃度提升）
+- level 3 管  理
+- level 4 群  主
+- level 5 主  人
+- level 6 维护组
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { GroupMessageEvent } from 'oicq';
-import { Extension, Bot, Order, getOption, getOrder, Option } from 'kokkoro-core';
+import { Extension, Bot, Order, getOption, getOrder, Option } from 'kokkoro';
 
 interface GroupOption extends Option {
   title_level: number;
 }
 
-export default class implements Extension {
+export default class Group implements Extension {
   bot: Bot;
   option: GroupOption = {
     title_level: 2,
   }
   orders: Order[] = [
     {
-      func: this.title,
+      func: this.applyTitle,
       regular: /^申请头衔[\s]?.+$/,
     },
   ];
@@ -27,21 +27,23 @@ export default class implements Extension {
     const order = getOrder(this.orders, raw_message);
 
     if (option.apply) {
-      order && order.func.call(this.bot, event, option);
+      order && order.call(this, event, option);
     }
   }
 
-  async title(this: Bot, event: GroupMessageEvent, option: GroupOption) {
-    const { uin, gl } = this;
+  async applyTitle(event: GroupMessageEvent, option: GroupOption) {
+    const { uin } = this.bot;
     const { group_id, raw_message, sender } = event;
     const { user_id } = sender;
-    const level = this.getUserLevel(event);
     const { title_level } = option;
+
+    const gl = this.bot.getGroupList();
+    const level = this.bot.getUserLevel(event);
 
     let message = null;
 
     switch (true) {
-      case gl.get(group_id)?.owner_id !== uin:
+      case gl.get(group_id)!.owner_id !== uin:
         message = `申请头衔需要 bot 拥有群主权限才能正常使用`;
         break;
       case level < title_level:
@@ -55,12 +57,11 @@ export default class implements Extension {
 
     const title = raw_message.replace('申请头衔', '').trim();
 
-    this.setGroupSpecialTitle(group_id, user_id, title)
-      .then(() => {
-        event.reply('申请成功', true);
-      })
-      .catch(() => {
-        event.reply('申请失败', true);
-      })
+    try {
+      await this.bot.setGroupSpecialTitle(group_id, user_id, title);
+      event.reply('申请成功', true);
+    } catch (error) {
+      event.reply('申请失败', true);
+    }
   }
 }
